a59de8f82a6c6599b08ea0cf073bbb701a192fc8
Doug Lau <doug.p.lau@gmail.com>
2026-03-18 15:02:41 -0500

    bulb: Simplified panic hook

--- bulb/src/panic.rs
+++ bulb/src/panic.rs
@@ -5,8 +5,7 @@
 
 cfg_if::cfg_if! {
     if #[cfg(target_arch = "wasm32")] {
-        extern crate wasm_bindgen;
-        use wasm_bindgen::prelude::*;
+        use wasm_bindgen::prelude::wasm_bindgen;
 
         #[wasm_bindgen]
         extern {
@@ -22,49 +21,20 @@ cfg_if::cfg_if! {
             fn stack(error: &Error) -> String;
         }
 
-        fn hook_impl(info: &std::panic::PanicHookInfo) {
+        fn hook(info: &std::panic::PanicHookInfo) {
             let mut msg = info.to_string();
-
-            // Add the error stack to our message.
-            //
-            // This ensures that even if the `console` implementation doesn't
-            // include stacks for `console.error`, the stack is still available
-            // for the user. Additionally, Firefox's console tries to clean up
-            // stack traces, and ruins Rust symbols in the process
-            // (https://bugzilla.mozilla.org/show_bug.cgi?id=1519569) but since
-            // it only touches the logged message's associated stack, and not
-            // the message's contents, by including the stack in the message
-            // contents we make sure it is available to the user.
             msg.push_str("\n\nStack:\n\n");
-            let e = Error::new();
-            let stack = e.stack();
-            msg.push_str(&stack);
-
-            // Safari's devtools, on the other hand, _do_ mess with logged
-            // messages' contents, so we attempt to break their heuristics for
-            // doing that by appending some whitespace.
+            msg.push_str(&Error::new().stack());
             msg.push_str("\n\n");
-
-            // Finally, log the panic with `console.error`!
             error(msg);
         }
     } else {
-        use std::io::{self, Write};
+        use std::io::Write;
 
-        fn hook_impl(info: &panic::PanicInfo) {
-            let _ = writeln!(io::stderr(), "{}", info);
+        fn hook(info: &panic::PanicHookInfo) {
+            let _ = writeln!(std::io::stderr(), "{}", info);
         }
     }
-}
-
-/// A panic hook for use with
-/// [`std::panic::set_hook`](https://doc.rust-lang.org/nightly/std/panic/fn.set_hook.html)
-/// that logs panics into
-/// [`console.error`](https://developer.mozilla.org/en-US/docs/Web/API/Console/error).
-///
-/// On non-wasm targets, prints the panic to `stderr`.
-fn hook(info: &std::panic::PanicHookInfo) {
-    hook_impl(info);
 }
 
 /// Set the `console.error` panic hook the first time this is called.
